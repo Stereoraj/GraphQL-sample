3e155c9160f519bdb6a7f0d76136bd3d3b016896
rajesh <merajesh04@gmail.com>
2019-05-15 01:48:56 +0530

    Add user model and hash password module

--- models/event.js
+++ models/event.js
@@ -18,6 +18,10 @@ const eventSchema = mongoose.Schema({
     date: {
         type: Date,
         required: true
+    },
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
     }
 });
 
