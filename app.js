74c063e5c19395ab2214b8e16ae96316c6e472a5
rajesh <merajesh04@gmail.com>
2019-06-26 02:14:37 +0530

    Make API call from the front-end

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if(req.method === "OPTIONS") {
+   	return res.sendStatus(200); 
+    }
+    next();
+});
+
 app.use(isAuth);
 
 app.use("/graphql", graphqlHttp({
@@ -25,8 +35,8 @@ console.log("connecting to the database ...");
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clusterevents-lvnzw.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`)
 .then(() => {
     console.log("Database connectivity successful !!");
-    app.listen(3000, () => {
-        console.log("Listening at port 3000 ...");
+    app.listen(4000, () => {
+        console.log("Listening at port 4000 ...");
     });  
 })
 .catch(err => {
